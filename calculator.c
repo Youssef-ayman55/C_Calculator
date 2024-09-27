8c83063e8620aec66550dcc6452a8e76c56be71e
Youssef_Ayman_Hassan <youssefayman55200@gmail.com>
2024-09-27 23:36:58 +0300

    Finished Frontend

--- calculator.c
+++ calculator.c
@@ -1,9 +1,30 @@
-#include <stdio.h>
 #include <gtk/gtk.h>
-#include <glfw/glfw3.h>
-#include <string.h>
-int main()
+
+static void
+print_hello(GtkWidget *widget,
+            gpointer data)
 {
-    printf("Hellow World");
-    return 0;
+    g_print("Hello World\n");
+}
+
+static void activate(GtkApplication *app, gpointer user_data)
+{
+    GtkBuilder *build;
+    build = gtk_builder_new_from_file("calculator.ui");
+    GtkWidget *win;
+    win = GTK_WIDGET(gtk_builder_get_object(build, "win"));
+    gtk_window_set_application(GTK_WINDOW(win), GTK_APPLICATION(app));
+    gtk_widget_show(GTK_WIDGET(win));
+}
+
+int main(int argc, char **argv)
+{
+    GtkApplication *app;
+    int status;
+
+    app = gtk_application_new("c.calculator", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+    status = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
+    return status;
 }
